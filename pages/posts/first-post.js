3aa41ea05e102f7058e51df1939898848383ce65
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-03-16 19:01:46 +0600

    blogs added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,29 @@
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 import React from "react";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
-    return (
-        <>
-            <Head>
-                <title>First Post!</title>
-            </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-        </>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>First Post!</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <button>hello</button>
+    </Layout>
+  );
 };
 
 export default FirstPost;
